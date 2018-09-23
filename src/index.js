00712a6a20f582a400d608cbb0ff4410d982bdd0
Jadgptw <Gordin.Alexander90@gmail.com>
2018-09-23 10:48:20 +0300

    index.js implementation has been added.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,88 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+      // your implementation
+        this.currentExpression = [initialValue];
+        this.operators = ['^', '*', '/', '-', '+'];
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    valueOf() {
+        return this.getCurrentResult();
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    getCurrentResult(){
+        let expression = this.currentExpression.slice(0);
+        for(let i = 0; i < this.operators.length; i++){
+            let index = this.findIndex(expression, this.operators[i]);
+            while(index >= 0){
+                expression.splice(index - 1, 3, this.makeOperation(expression[index - 1],
+                    expression[index + 1],
+                    this.operators[i]));
+                index = this.findIndex(expression, this.operators[i]);
+            }
+        }
+        return expression[0];
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    findIndex (expression, operator){
+        if (operator === '^'){
+            return expression.lastIndexOf(operator);
+        }
+        else{
+            return expression.indexOf(operator);
+        }
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    makeOperation(a, b, operator){
+        switch(operator){
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+            case '^':
+                return Math.pow(a, b);
+        }
+    }
+
+    add(number) {
+      // your implementation
+        this.currentExpression.push('+');
+        this.currentExpression.push(number);
+        return this;
+    }
+
+    subtract(number) {
+      // your implementation
+        this.currentExpression.push('-');
+        this.currentExpression.push(number);
+        return this;
+    }
+
+    multiply(number) {
+      // your implementation
+        this.currentExpression.push('*');
+        this.currentExpression.push(number);
+        return this;
+    }
+
+    devide(number) {
+      // your implementation
+        this.currentExpression.push('/');
+        this.currentExpression.push(number);
+        return this;
+    }
+
+    pow(number) {
+      // your implementation
+        this.currentExpression.push('^');
+        this.currentExpression.push(number);
+        return this;
+    }
+
+
 }
 
 module.exports = SmartCalculator;
